c3e27d8bd22c89931c2e43db80234ade6f173498
Sbarrachina <sandrabarrachina1978@gmail.com>
2021-11-08 12:21:12 +0100

    añadir javascript,listadoCoders

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+let listaCoders = [ {name:"LauraCa",status:"alive"},
+{name:"Sandra",status:"alive"},
+{name:"Valentina",status:"alive"},
+{name:"Tamara",status:"alive"},
+{name:"Judith",status:"alive"},
+{name:"Helen",status:"alive"},
+{name:"Carmen",status:"alive"},
+{name:"Candy",status:"alive"},
+{name:"Gabrielle",status:"alive"},
+{name:"Yulilla",status:"alive"},
+{name:"Anna G",status:"alive"},
+{name:"Sonia",status:"alive"},
+{name:"Desirée",status:"alive"},
+{name:"Alisa",status:"alive"},
+{name:"Faby",status:"alive"},
+{name:"Ana C",status:"alive"},
+{name:"Joana",status:"alive"},
+{name:"Gracia",status:"alive"},
+{name:"Alexia",status:"alive"},
+{name:"Marisa",status:"alive"},
+{name:"Laura C",status:"alive"},
+{name:"Rosa",status:"alive"},
+{name:"Kristina",status:"alive"},
+];
