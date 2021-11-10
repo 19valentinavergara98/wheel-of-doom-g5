280fb65a0d716d67bcf666f990581f37bf0f39dc
RosaPs <rosapacheco.contact@gmail.com>
2021-11-10 13:49:32 +0100

    muestra de nombre array

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
 
-let listaCoders = [ {name:"LauraC",status:"alive"},
-    {name:"Sandra",status:"alive"},
-    {name:"Valentina",status:"alive"},
-    {name:"Tamara",status:"alive"},
-    {name:"Judith",status:"alive"},
-    {name:"Helen",status:"alive"},
-    {name:"Carmen",status:"alive"},
-    {name:"Candy",status:"alive"},
-    {name:"Gabrielle",status:"alive"},
-    {name:"Yulilla",status:"alive"},
-    {name:"Anna G",status:"alive"},
-    {name:"Sonia",status:"alive"},
-    {name:"Desirée",status:"alive"},
-    {name:"Alisa",status:"alive"},
-    {name:"Faby",status:"alive"},
-    {name:"Ana C",status:"alive"},
-    {name:"Joana",status:"alive"},
-    {name:"Gracia",status:"alive"},
-    {name:"Alexia",status:"alive"},
-    {name:"Marisa",status:"alive"},
-    {name:"Laura M",status:"alive"},
-    {name:"Rosa",status:"alive"},
-    {name:"Kristina",status:"alive"},
+let listaCoders = [ "LauraC",
+    "Sandra",
+    "Valentina",
+    "Tamara",
+    "Judith",
+    "Helen",
+    "Carmen",
+    "Candy",
+    "Gabrielle",
+    "Yulilla",
+    "Anna G",
+    "Sonia",
+    "Desirée",
+    "Alisa",
+    "Faby",
+    "Ana C",
+    "Joana",
+    "Gracia",
+    "Alexia",
+    "Marisa",
+    "Laura M",
+    "Rosa",
+    "Kristina",
     ];
 function elegirCoders(){
-    
 
-    let coderElegida = Math.floor(Math.random()*listaCoders.length);
+   
+    let coderElegida = listaCoders[Math.floor(Math.random()*listaCoders.length)];
     document.getElementById('startButton').innerHTML=' <section class="mostrarnombre"><p>nombre:</p></section>'+coderElegida;
     
 
 }
+
+
+
+
+
+
+
+
 
